406e4af9c2e66a85a393e3543a32111b40d83fe4
ubuntu <ubuntu@localhost.localdomain>
2019-03-14 15:49:13 +0800

    add a script autopull codes from git hub and add cpp solution directory

---
+++ autopull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "download python3 codes..."
+leetcode pull -a -l python3 -o solution-python3
+
+echo "download cpp codes..."
+leetcode pull -a -l cpp -o solution-cpp
---
+++ solution-cpp/1.two-sum.214724384.ac.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (42.35%)
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash_table;
+
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = hash_table.find(target - nums[i]);
+            if (it != hash_table.end()) return {it->second, i};
+            hash_table[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
