1acf156d95d9b26eed5844aff562be981f3fac3e
callmexss <callmexss@126.com>
2019-03-20 21:57:21 +0800

    update solutions

---
+++ solution-python3/58.length-of-last-word.215963022.ac.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=58 lang=python3
+#
+# [58] Length of Last Word
+#
+# https://leetcode.com/problems/length-of-last-word/description/
+#
+# algorithms
+# Easy (32.17%)
+# Total Accepted:    250.8K
+# Total Submissions: 779.6K
+# Testcase Example:  '"Hello World"'
+#
+# Given a string s consists of upper/lower-case alphabets and empty space
+# characters ' ', return the length of last word in the string.
+# 
+# If the last word does not exist, return 0.
+# 
+# Note: A word is defined as a character sequence consists of non-space
+# characters only.
+# 
+# Example:
+# 
+# Input: "Hello World"
+# Output: 5
+# 
+# 
+#
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        return len(s) - (s.rfind(' ') + 1)
+
---
+++ solution-python3/9.palindrome-number.215500496.ac.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (42.24%)
+# Total Accepted:    527.1K
+# Total Submissions: 1.2M
+# Testcase Example:  '121'
+#
+# Determine whether an integer is a palindrome. An integer is a palindrome when
+# it reads the same backward as forward.
+# 
+# Example 1:
+# 
+# 
+# Input: 121
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+# 
+# 
+# Follow up:
+# 
+# Coud you solve it without converting the integer to a string?
+# 
+#
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        start = 0
+        end = len(s) - 1
+        while start <= end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+        
+
